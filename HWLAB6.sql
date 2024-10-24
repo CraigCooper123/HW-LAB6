8c7938cce33eca1d276da4cc9924aa651b221653
CraigCooper123 <166830299+CraigCooper123@users.noreply.github.com>
2024-10-25 00:12:51 +0700

    Add files via upload

---
+++ HWLAB6.sql
@@ -0,0 +1,41 @@
+USE northwindfulldata;
+# 1
+# SELECT * FROM Customers
+# WHERE Country = 'Mexico';
+
+# 2
+# SELECT DISTINCT Country FROM Customers;
+
+# 3
+# SELECT * FROM Customers
+# WHERE Country = 'Mexico' or 'Canada' or 'USA';
+
+# 4
+# SELECT CustomerName FROM Customers
+# WHERE City != 'San Francisco'
+
+# 5
+# SELECT ProductName AS Name;
+
+# 6
+# SELECT * FROM Customers ORDER BY Price DESC;
+
+# 7
+# SELECT * FROM Customers ORDER BY Price DESC LIMIT 5;
+
+# 8
+# SELECT Unit FROM Customers
+# WHERE Name LIKE 'boxes%' AND Price BETWEEN $10 AND $25;
+
+# 10
+# SELECT * FROM Customers
+# WHERE ProductID > 10 AND Quantity > 10;
+
+
+
+
+
+
+
+
+
